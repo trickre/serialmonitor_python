77c431e7fe87eaeb9fae73b669aebbff54f99844
tricker <ogino.ktai@gmail.com>
2018-08-17 05:24:59 +0900

    add description for read_untill()

--- partner_arduino/partner_arduino.ino
+++ partner_arduino/partner_arduino.ino
@@ -1,9 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("HelloSerialMonitor!");
+  int i = 0;
+  Serial.print("#");//区切り文字として
+  for (i = 0; i < 10; i++) {
+    Serial.print("HelloSerialMonitor!");
+  }
 }
